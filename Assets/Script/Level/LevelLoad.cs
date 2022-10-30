3fc9c5830e7c623c30692ea3243c811d0364b051
TrickSlayer <90195916+TrickSlayer@users.noreply.github.com>
2022-10-30 13:38:00 +0700

    camera scale

--- Assets/Script/Level/LevelLoad.cs
+++ Assets/Script/Level/LevelLoad.cs
@@ -2,20 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static ObjectPooler;
 
-public class LoadLevel : MonoBehaviour
+public class LevelLoad : MonoBehaviour
 {
     public string sLevelToLoad;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             StartCoroutine(LoadScreen(sLevelToLoad));
         }
-            
+
     }
 
     public static IEnumerator LoadScreen(string name)
